8fb250523ae85ecc794abaa5c2c6212c8016472c
DianaGaleano245 <emmaymaggi@gmail.com>
2021-10-21 15:10:20 -0300

    primer commit

---
+++ Software.Core/Proyecto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Software.Core
+{
+    public class Proyecto
+    {
+        public string Descripcion { get; set; }
+        public Decimal Presupuesto { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fin { get; set; }
+        public string Cliente { get; set; }
+    }
+}
